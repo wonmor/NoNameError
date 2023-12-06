14f933c1b68adbb38c46a3879f496122d5954712
John Seong <wonmor@gmail.com>
2023-12-05 16:10:41 -0800

    Update john.m

--- john.m
+++ john.m
@@ -5,6 +5,18 @@ inches = input('Enter your height in inches: ');
 % Input weight in pounds
 weight = input('Enter your weight in pounds: ');
 
+gender = input('Enter your gender (M/F): ', 's'); % 's' indicates input as a string
+
+% Input age in years
+age = input('Enter your age in years: '); % Add this line to collect the user's age
+
+% Calculate BMR (Basal Metabolic Rate)
+if strcmpi(gender, 'm') % Use strcmpi to compare gender case-insensitively
+    bmr = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age);
+else
+    bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
+end
+
 % Convert height to inches
 height = (feet * 12) + inches;
 
@@ -13,32 +25,53 @@ bmi = (weight / (height^2)) * 703;
 
 % Display the BMI
 fprintf('Your BMI is: %.2f\n', bmi);
+fprintf('Your BMR is: %.2f\n', bmr);
 
-% Interpret the BMI
-if bmi < 18.5
-    fprintf('You are underweight.\n');
-    dietRecommendation = 'You should consider increasing your calorie intake with a balanced diet.';
-elseif bmi >= 18.5 && bmi < 24.9
-    fprintf('Your weight is in the healthy range.\n');
-    dietRecommendation = 'Continue with your current diet and exercise routine to maintain a healthy weight.';
-elseif bmi >= 25 && bmi < 29.9
-    fprintf('You are overweight.\n');
-    dietRecommendation = 'Consider reducing your calorie intake and increasing physical activity.';
+% Calculate BMR (Basal Metabolic Rate)
+if gender == 'm' || gender == 'M'
+    bmr = 66 + (6.23 * weight) + (12.7 * height) - (6.8 * age);
 else
-    fprintf('You are obese.\n');
-    dietRecommendation = 'It is essential to consult a healthcare professional for a personalized weight management plan.';
+    bmr = 655 + (4.35 * weight) + (4.7 * height) - (4.7 * age);
 end
 
-% Display diet recommendation
-fprintf('Diet Recommendation: %s\n', dietRecommendation);
+% Define activity levels and their corresponding multipliers
+activityLevels = {
+    'Sedentary (little or no exercise)', 1.2;
+    'Lightly active (light exercise/sports 1-3 days/week)', 1.375;
+    'Moderately active (moderate exercise/sports 3-5 days/week)', 1.55;
+    'Very active (hard exercise/sports 6-7 days a week)', 1.725;
+    'Super active (very hard exercise & physical job)', 1.9;
+};
 
-% Create a chart to visualize BMI
-x = categorical({'Underweight', 'Healthy Weight', 'Overweight', 'Obese'});
-y = [bmi, 24.9, 29.9, 50]; % Set upper bounds for each category
+% Prompt the user to select their activity level
+fprintf('Select your activity level:\n');
+for i = 1:length(activityLevels)
+    fprintf('%d. %s\n', i, activityLevels{i, 1});
+end
+
+activityLevelIndex = input('Enter the number corresponding to your activity level: ');
+
+% Calculate TDEE (Total Daily Energy Expenditure)
+tdee = bmr * activityLevels{activityLevelIndex, 2};
+
+% Define calorie intake goals (for maintaining, losing, and gaining weight)
+calorieIntakeMaintain = tdee;
+calorieIntakeLose = tdee - 500; % Aim for a calorie deficit of 500 calories/day for weight loss
+calorieIntakeGain = tdee + 500; % Aim for a calorie surplus of 500 calories/day for weight gain
+
+% Display calorie intake recommendations
+fprintf('Calorie Intake Recommendations:\n');
+fprintf('To maintain weight: %.2f calories/day\n', calorieIntakeMaintain);
+fprintf('To lose weight: %.2f calories/day\n', calorieIntakeLose);
+fprintf('To gain weight: %.2f calories/day\n', calorieIntakeGain);
+
+% Create a bar chart to visualize calorie intake recommendations
+x = categorical({'Maintain Weight', 'Lose Weight', 'Gain Weight'});
+y = [calorieIntakeMaintain, calorieIntakeLose, calorieIntakeGain];
 bar(x, y, 'b');
-title('BMI Chart');
-xlabel('BMI Categories');
-ylabel('BMI Value');
+title('Calorie Intake Recommendations');
+xlabel('Weight Management Goals');
+ylabel('Calories per Day');
 grid on;
 
 % Define a large dataset of food recommendations
